43bf4fbaffd9e6a3e51bfd52b52a45aad6003a9d
ryanwhocodes <22416807+ryanwhocodes@users.noreply.github.com>
2018-06-23 19:17:02 +0100

    Clean commits

---
+++ docs/extensions.md
@@ -0,0 +1,20 @@
+# Extensions
+
+### [Package your extension - Mozilla | MDN](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Package_your_extension_)
+- Open Terminal.
+- Open the directory containing your extension's files,  using the command
+`cd path/to/my-extension/``
+- ZIP the content of the directory, using the command
+`zip -r -FS ../my-extension.zip *`
+
+### Resources
+
+- [Debugging with Firefox Developer Tools - about:debugging](about:debugging)
+- [Chrome Extensions - chrome://extensions/](chrome://extensions/)
+- [Imagemagick to create favicon.ico with 16x16 and 32x32 sizes in it](https://gist.github.com/nateware/900d2d09f4884ac0c073)
+- [audreyr/favicon-cheat-sheet: Obsessive cheat sheet to favicon sizes/types](https://github.com/audreyr/favicon-cheat-sheet)
+- [Using media queries - CSS: Cascading Style Sheets | MDN](https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Using_media_queries)
+- [manifest.json - Mozilla | MDN](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/manifest.json)
+- [Manifest File Format - Google Chrome](https://developer.chrome.com/apps/manifest)
+- [Your first extension - Mozilla | MDN](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Your_first_WebExtension)
+- [Browser Extensions - Mozilla | MDN](https://developer.mozilla.org/en-US/Add-ons/WebExtensions)
---
+++ extension/js/popup.js
@@ -0,0 +1,69 @@
+// Link Formatter js
+
+// hide the alert that will be triggered if there is a failure to copy text
+$('#alert').hide()
+
+// to close the alert
+$("#alert").click(() => { $('#alert').hide() });
+
+// updating the formatted links when user amends link text or url
+$( "#textInput, #urlInput" ).on("keyup change", () => { updateLinkFormats() });
+
+// Copy button events, obtains the input id from amending the button id
+$("button").click((event) => {
+  let inputId = "#" + event.target.id.replace("CopyButton", "Input")
+  copyInputToClipboard(inputId)
+});
+
+const copyInputToClipboard = (elementId) => {
+  let data = $(elementId).val()
+  let dt = new clipboard.DT();
+  dt.setData("text/plain", data);
+  clipboard.write(dt)
+  .then(() => {
+    // confirm successful copy
+    $('[data-toggle="popover"]').popover('disable').popover('hide')
+    $(elementId).popover('enable').popover('show')
+    setTimeout(() => { $(elementId).popover('disable').popover('hide') }, 2000)
+    $(elementId).select()
+  })
+  .catch((error) => {
+    alertError(error, "Error occurred when trying to copy to clipboard")
+  })
+}
+
+const alertError = (error, message) => {
+  console.log(message)
+  console.log(error)
+  $('#alertText').text(message)
+  $('#alert').show()
+}
+
+const updateLinkFormats = () => {
+  let linkText = $('#textInput').val();
+  let linkUrl = $('#urlInput').val();
+  $('#htmlLinkInput').val("<a href=\"" + linkUrl + "\">" + linkText + "</a>")
+  $('#markdownLinkInput').val("[" + linkText + "](" + linkUrl + ")")
+}
+
+const addLinkDataFromTab = (tabs) => {
+  currentTab = tabs[0]
+  $('#textInput').val(currentTab.title);
+  $('#urlInput').val(currentTab.url);
+  updateLinkFormats()
+}
+
+// To enable cross-browser use you need to see if this is Chrome or not
+if(chrome) {
+  chrome.tabs.query(
+    {active: true, currentWindow: true},
+    (arrayOfTabs) => { addLinkDataFromTab(arrayOfTabs) }
+  );
+  // This enables links to be opened in new tabs
+  $('a').click( (event) => { chrome.tabs.create({url:event.target.href}) } )
+} else {
+  browser.tabs.query({active: true, currentWindow: true})
+    .then(addLinkDataFromTab)
+  // This enables links to be opened in new tabs
+  $('a').click( (event) => { browser.tabs.create({url:event.target.href}) } )
+}
